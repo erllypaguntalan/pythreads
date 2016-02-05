3b5fc56b91b9d2945dffcc14f18bb9122fc46d1e
erllypaguntalan <e.paguntalan@klab.com>
2016-02-05 10:37:17 +0800

    View own profile

--- app/models.py
+++ app/models.py
@@ -2,14 +2,18 @@ from datetime import datetime
 
 from app import db
 
-
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.String, nullable=False, primary_key=True)
-    created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    updated = db.Column(db.DateTime, default=datetime.utcnow, nullable=False,
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    date_updated = db.Column(db.DateTime, default=datetime.utcnow, nullable=False,
                         onupdate=datetime.utcnow)
     name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String(64), nullable=True, index=True, unique=True)
+    about_me = db.Column(db.String(200), nullable=True)
     profile_url = db.Column(db.String, nullable=False)
     access_token = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % (self.name)
--- app/views.py
+++ app/views.py
@@ -1,10 +1,9 @@
 from facebook import get_user_from_cookie, GraphAPI
-from flask import g, render_template, redirect, request, session, url_for
+from flask import g, flash, render_template, redirect, request, session, url_for
 
 from app import app, db
 from config import FB_APP_ID, FB_APP_NAME, FB_APP_SECRET
 from .models import User
-
 
 @app.route('/')
 def index():
@@ -19,7 +18,6 @@ def index():
 def login():
     return render_template('login.html', app_id=FB_APP_ID, name=FB_APP_NAME)
 
-
 @app.route('/logout')
 def logout():
     """Log out the user from the application.
@@ -29,7 +27,6 @@ def logout():
     """
     session.pop('user', None)
     return redirect(url_for('index'))
-
 
 @app.before_request
 def get_current_user():
@@ -47,7 +44,8 @@ def get_current_user():
             profile = graph.get_object('me')
             if 'link' not in profile:
                 profile['link'] = ""
-            user = User(id=str(profile['id']), name=profile['name'],
+            user = User(id=str(profile['id']),
+                        name=profile['name'],
                         profile_url=profile['link'],
                         access_token=result['access_token'])
             db.session.add(user)
@@ -57,3 +55,17 @@ def get_current_user():
                                id=user.id, access_token=user.access_token)
     db.session.commit()
     g.user = session.get('user', None)
+
+@app.route('/user/<name>')
+def user(name):
+    user = User.query.filter_by(name=name).first()
+    if user == None:
+        flash('User %s not found.' % name)
+        return redirect(url_for('index'))
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html',
+                           user=user,
+                           posts=posts)
