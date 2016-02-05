3c85a360b05e34a30b42ec10b924e44f7f965bd5
erllypaguntalan <e.paguntalan@klab.com>
2016-02-05 14:55:49 +0800

    View other's profile

--- app/models.py
+++ app/models.py
@@ -10,10 +10,13 @@ class User(db.Model):
     date_updated = db.Column(db.DateTime, default=datetime.utcnow, nullable=False,
                         onupdate=datetime.utcnow)
     name = db.Column(db.String, nullable=False)
-    email = db.Column(db.String(64), nullable=True, index=True, unique=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
     about_me = db.Column(db.String(200), nullable=True)
-    profile_url = db.Column(db.String, nullable=False)
     access_token = db.Column(db.String, nullable=False)
 
+    @property
+    def is_authenticated(self):
+        return True
+
     def __repr__(self):
-        return '<User %r>' % (self.name)
+        return '<User %r>' % (self.nickname)
--- app/views.py
+++ app/views.py
@@ -5,31 +5,19 @@ from app import app, db
 from config import FB_APP_ID, FB_APP_NAME, FB_APP_SECRET
 from .models import User
 
+
+
 @app.route('/')
 def index():
-    return render_template('index.html', app_id=FB_APP_ID, app_name=FB_APP_NAME, user=g.user)
+    if g.user:
+        return render_template('index.html', app_id=FB_APP_ID,
+                                app_name=FB_APP_NAME, user=g.user)
+            
+    return render_template('index.html', app_id=FB_APP_ID, name=FB_APP_NAME)
 
-    # if g.user:
-    #     return render_template('index.html', app_id=FB_APP_ID,
-    #                            app_name=FB_APP_NAME, user=g.user)
-    # return render_template('login.html', app_id=FB_APP_ID, name=FB_APP_NAME)
-
-@app.route('/login')
-def login():
-    return render_template('login.html', app_id=FB_APP_ID, name=FB_APP_NAME)
-
-@app.route('/logout')
-def logout():
-    """Log out the user from the application.
-    Log out the user from the application by removing them from the
-    session.  Note: this does not log the user out of Facebook - this is done
-    by the JavaScript SDK.
-    """
-    session.pop('user', None)
-    return redirect(url_for('index'))
 
 @app.before_request
-def get_current_user():
+def before_request():
     if session.get('user'):
         g.user = session.get('user')
         return
@@ -42,25 +30,40 @@ def get_current_user():
         if not user:
             graph = GraphAPI(result['access_token'])
             profile = graph.get_object('me')
-            if 'link' not in profile:
-                profile['link'] = ""
-            user = User(id=str(profile['id']),
-                        name=profile['name'],
-                        profile_url=profile['link'],
-                        access_token=result['access_token'])
+
+
+            user = User(id=str(profile['id']), name=profile['name'],
+                        access_token=result['access_token'],
+                        nickname=profile['name'].split(' ')[0])
             db.session.add(user)
         elif user.access_token != result['access_token']:
             user.access_token = result['access_token']
-        session['user'] = dict(name=user.name, profile_url=user.profile_url,
-                               id=user.id, access_token=user.access_token)
+
+        session['user'] = dict(name=user.name, id=user.id, access_token=user.access_token,
+                                nickname=user.nickname)
     db.session.commit()
     g.user = session.get('user', None)
 
-@app.route('/user/<name>')
-def user(name):
-    user = User.query.filter_by(name=name).first()
+
+
+@app.route('/login')
+def login():
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('index'))
+    return render_template('login.html', app_id=FB_APP_ID, name=FB_APP_NAME)
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('index'))
+
+
+
+@app.route('/user/<nickname>')
+def user(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
     if user == None:
-        flash('User %s not found.' % name)
+        flash('User %s not found.' % nickname)
         return redirect(url_for('index'))
     posts = [
         {'author': user, 'body': 'Test post #1'},
